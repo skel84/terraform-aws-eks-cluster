3c8191849fb8a760fcba8d3da012205ab9005032
Francesco Cislaghi <skel84@users.noreply.github.com>
2021-01-15 12:57:18 +0100

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "assume_role" {
 
     principals {
       type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
+      identifiers = ["eks.amazonaws.com.cn"]
     }
   }
 }
