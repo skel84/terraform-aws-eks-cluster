f208ac5cb1e466c547e54c96788665cc6ea779ef
Francesco Cislaghi <skel84@users.noreply.github.com>
2021-01-26 12:10:18 +0100

    Update iam.tf
    remove .cn from eks domain name

--- iam.tf
+++ iam.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "assume_role" {
 
     principals {
       type        = "Service"
-      identifiers = ["eks.amazonaws.com.cn"]
+      identifiers = ["eks.amazonaws.com"]
     }
   }
 }
